e1af43553c68c382e0ec3009b3e2a8a99a9ba296
Stanislav Filenko <s.filenko16@gmail.com>
2022-06-18 18:58:03 +0300

    Code fixed

--- app.py
+++ app.py
@@ -1,11 +1,53 @@
-from flask import Flask
+from flask import Flask, request
+from faker import Faker
+import csv
+import requests
 
 app = Flask(__name__)
+fake = Faker()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
+
+
+@app.route('/requirements/')
+def requirements():
+    with open('requirements.txt', 'r', encoding='UTF-8') as text:
+        return text.read()
+
+
+@app.route('/generate-users/')
+def user_generate():
+    value = request.args.get('count', 100)
+    handbook = {}
+    for _ in range(int(value)):
+        person = {fake.name(): fake.email()}
+        handbook.update(person)
+    return handbook
+
+
+@app.route('/mean/')
+def parameters():
+    with open('hw.csv', newline='') as csvfile:
+        lines = csv.reader(csvfile.readlines()[1:25001], delimiter=',')
+        height_inches = 0
+        weight_pounds = 0
+        for row in lines:
+            height_inches += float(row[1])
+            weight_pounds += float(row[2])
+        height_cm = round(height_inches * 2.54, 2)
+        weight_kg = round(weight_pounds / 2.2046, 2)
+    return f'Average weight in Kg: {weight_kg} ' \
+           f'Average height in CM:{height_cm}'
+
+
+@app.route('/space/')
+def astronaut():
+    response = requests.get('http://api.open-notify.org/astros.json')
+    number = response.json()['number']
+    return f'{str(number)} astronauts are in space now'
 
 
 if __name__ == '__main__':
