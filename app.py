9bab68f2ba1402bd5809e18c510204360e5fd088
Stanislav Filenko <s.filenko16@gmail.com>
2022-06-18 20:38:04 +0300

    Final code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from faker import Faker
 import csv
 import requests
@@ -9,22 +9,28 @@ fake = Faker()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello.html')
 
 
 @app.route('/requirements/')
 def requirements():
     with open('requirements.txt', 'r', encoding='UTF-8') as text:
-        return text.read()
+        lst = text.readlines()
+        lines = tuple(map(lambda value: value.strip('\n'), lst))
+        return render_template('parameters.html', line=lines)
 
 
 @app.route('/generate-users/')
 def user_generate():
     value = request.args.get('count', 100)
     handbook = {}
+    template_name = '{name:<20}'
+    template_email = '{email:<15}'
     for _ in range(int(value)):
-        person = {fake.name(): fake.email()}
-        handbook.update(person)
+        person = fake.name()
+        email = fake.email()
+        line = {template_name.format(name=person): template_email.format(email=email)}
+        handbook.update(line)
     return handbook
 
 
@@ -39,15 +45,14 @@ def parameters():
             weight_pounds += float(row[2])
         height_cm = round(height_inches * 2.54, 2)
         weight_kg = round(weight_pounds / 2.2046, 2)
-    return f'Average weight in Kg: {weight_kg} ' \
-           f'Average height in CM:{height_cm}'
+    return render_template('parameters.html', weight=weight_kg, height=height_cm)
 
 
 @app.route('/space/')
 def astronaut():
     response = requests.get('http://api.open-notify.org/astros.json')
     number = response.json()['number']
-    return f'{str(number)} astronauts are in space now'
+    return render_template('parameters.html', number=number)
 
 
 if __name__ == '__main__':
